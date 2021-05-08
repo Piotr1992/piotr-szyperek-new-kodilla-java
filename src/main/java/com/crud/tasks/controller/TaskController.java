22ede9ee58addf6d276b042720321b0991685ccc
Lenovo <szyperek.piotr@o2.pl>
2021-05-08 02:04:42 +0200

    Task-32.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -4,11 +4,10 @@ import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//@RequestMapping("/v1/task")
 @RestController
-@RequestMapping("/v1/task")
 @CrossOrigin("*")
 public class TaskController {
 
