1b53be76af8dd732a102570e58f4f94e6a6bf8d1
Lenovo <szyperek.piotr@o2.pl>
2021-05-08 02:22:45 +0200

    Task-32.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,13 +1,19 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RequestMapping("/v1/task")
+import java.util.List;
+
 @RestController
+@RequestMapping("/v1/task")
 @CrossOrigin("*")
 public class TaskController {
 
@@ -20,11 +26,11 @@ public class TaskController {
         this.taskMapper = taskMapper;
     }
 
-/*    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
         List<Task> tasks = service.getAllTasks();
         return taskMapper.mapToTaskDtoList(tasks);
-    }               */
+    }
 
 /*    @RequestMapping(method = RequestMethod.POST, value = "createTask", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void createTask(@RequestBody TaskDto taskDto) {
