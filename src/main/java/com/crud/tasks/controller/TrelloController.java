da25adb9075a963bd650c5dd6ff2494be14b7a75
Lenovo <szyperek.piotr@o2.pl>
2021-05-09 00:09:41 +0200

    Task-32.2

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -33,8 +33,8 @@ public class TrelloController {
             new Mail(
                     "szyperekpiotr1992@gmail.com",
                     "Piotr Szyperek",
-                    "Subject",
-                    "Message"
+                    "Subject new",
+                    "Message new"
                 )
         );
 
