6f9c12aeeb43cf39dee9f14d6daad24e7c119504
Lenovo <szyperek.piotr@o2.pl>
2021-05-09 02:55:44 +0200

    Task-32.2

--- src/main/java/com/crud/tasks/mapper/TrelloMapper.java
+++ src/main/java/com/crud/tasks/mapper/TrelloMapper.java
@@ -15,18 +15,18 @@ public class TrelloMapper {
 /*    public List<TrelloBoard> mapToBoards(final List<TrelloBoardDto> trelloBoardDto) {
         return trelloBoardDto.stream()
                 .map(trelloBoard ->
-                        new TrelloBoard(trelloBoard.getId(), trelloBoard.getName(), mapToList(trelloBoard.getLists())))
-                .collect(toList());
-
-    }
-
-    public List<TrelloBoardDto> mapToBoardsDto(final List<TrelloBoard> trelloBoards) {
-        return trelloBoards.stream()
-                .map(trelloBoard ->
-                        new TrelloBoardDto(trelloBoard.getId(), trelloBoard.getName(), mapToListDto(trelloBoard.getLists())))
+                        new TrelloBoard(trelloBoardDto.getId(), trelloBoard.getName(), mapToListDto(trelloBoards)))
                 .collect(toList());
 
     }           */
+
+/*    public List<TrelloBoardDto> mapToBoardsDto(final List<TrelloBoard> trelloBoards) {
+        return trelloBoards.stream()
+                .map(trelloBoard ->
+                        new TrelloBoard(trelloBoard.getId(), trelloBoard.getName(), trelloBoard.getLists()))
+                .collect(toList());
+
+    }               */
 
     public List<TrelloList> mapToList(final List<TrelloListDto> trelloListDto) {
         return trelloListDto.stream()
@@ -34,11 +34,11 @@ public class TrelloMapper {
                 .collect(toList());
     }
 
-/*    public List<TrelloList> mapToListDto(final List<TrelloList> trelloLists) {
+    public List<TrelloList> mapToListDto(final List<TrelloList> trelloLists) {
         return trelloLists.stream()
-                .map(trelloList -> new TrelloListDto(trelloList.getId(), trelloList.getName(), trelloList.isClosed()))
+                .map(trelloList -> new TrelloList(trelloList.getId(), trelloList.getName(), trelloList.isClosed()))
                 .collect(toList());
-    }           */
+    }
 
     public TrelloCardDto mapToCardDto(final TrelloCard trelloCard) {
         return new TrelloCardDto(trelloCard.getName(), trelloCard.getDescription(), trelloCard.getPos(), trelloCard.getListId());
