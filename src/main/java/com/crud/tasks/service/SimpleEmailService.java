be7d84c094bf8915183c611074742272948f033f
Lenovo <szyperek.piotr@o2.pl>
2021-05-11 21:24:51 +0200

    Task 32.3 - Create template and send e-mail.

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -27,10 +27,6 @@ public class SimpleEmailService {
     public void send(final Mail mail) {
         log.info("Starting email preparation...");
         try {
-//            SimpleMailMessage mailMessage = createMailMessage(mail);
-            MimeMessagePreparator mailMessageCreateTrelloCard = createMimeMessageCreateCardTrello(mail);
-            javaMailSender.send(mailMessageCreateTrelloCard);
-//            javaMailSender.send(mailMessage2);
             MimeMessagePreparator mailMessageOnceDay = createMimeMessageOnceDay(mail);
             javaMailSender.send(mailMessageOnceDay);
             log.info("Email has been sent.");
@@ -52,46 +48,12 @@ public class SimpleEmailService {
         }
     }
 
-/*    private SimpleMailMessage createMailMessage(final Mail mail) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-
-        Optional<SimpleMailMessage> optionalMail = Optional.ofNullable(mailMessage);
-
-        mailMessage.setTo(mail.getMail());
-        if ( optionalMail.isPresent() ) {
-            mailMessage.setCc();
-        }
-        mailMessage.setSubject(mail.getSubject());
-        mailMessage.setText(mail.getMessage());
-
-        return mailMessage;
-    }                   */
-
-    private MimeMessagePreparator createMimeMessageCreateCardTrello(final Mail mail) {
-        return mimeMessage -> {
-            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
-            messageHelper.setTo(mail.getMail());
-            messageHelper.setSubject(mail.getSubject());
-            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
-        };
-    }
-
-    private SimpleMailMessage createMailMessageCreateCardTrello(final Mail mail) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(mail.getMail());
-        mailMessage.setSubject(mail.getSubject());
-        mailMessage.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()));
-        return mailMessage;
-    }
-
     private MimeMessagePreparator createMimeMessageOnceDay(final Mail mail) {
-        long size = taskRepository.count();
-        String messagetext = "Currently in database you got: " + size + ( (size == 1) ? " task" : " tasks").toString();
         return mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
             messageHelper.setTo(mail.getMail());
             messageHelper.setSubject(mail.getSubject());
-            messageHelper.setText(mailCreatorService.onceDayCountTasksEmail(messagetext), true);
+            messageHelper.setText(mailCreatorService.onceDayCountTasksEmail(mail.getMessage()), true);
         };
     }
 
--- src/main/java/com/crud/tasks/service/TrelloService.java
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -1,11 +1,9 @@
 package com.crud.tasks.service;
 
-import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.domain.CreatedTrelloCardDto;
 import com.crud.tasks.domain.Mail;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
-import com.crud.tasks.scheduler.EmailScheduler;
 import com.crud.tasks.trello.client.TrelloClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,17 +16,10 @@ public class TrelloService {
     private static final String SUBJECT = "Tasks: New Trello card";
     private final TrelloClient trelloClient;
     private final SimpleEmailService emailService;
-    private final AdminConfig adminConfig;
-
-    private final EmailScheduler emailScheduler;
 
     public List<TrelloBoardDto> fetchTrelloBoards() {
         return trelloClient.getTrelloBoards();
     }
-
-/*    public CreatedTrelloCardDto createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
-        return trelloService.createTrelloCard(trelloCardDto);
-    }           */
 
     public CreatedTrelloCardDto createTrelloCard(final TrelloCardDto trelloCardDto) {
         CreatedTrelloCardDto newCard = trelloClient.createNewCard(trelloCardDto);
@@ -41,15 +32,6 @@ public class TrelloService {
                         "Message"
                 )
         );
-
-/*        Optional.ofNullable(newCard).ifPresent(card -> emailService.send(
-            new Mail(
-                adminConfig.getAdminMail(),
-                null,
-                SUBJECT,
-                "New card: " + trelloCardDto.getName() + " has been created on your Trello account"
-            )
-        ));             */
 
         return newCard;
     }
